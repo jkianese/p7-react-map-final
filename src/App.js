b75916864facf6d52319f3e247af646be73f538d
[jkianese] <[jkianese@gmail.com]>
2018-10-11 22:43:37 -0400

    Map Rendered

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+
+  state = {
+    venues: [],
+    markers: [],
+    query: '',
+    infowindow: []
+  }  
+
+  componentDidMount() {
+    this.getVenues()
+    window.initMap = this.initMap
+  }
+
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id: "F1BIU3KU3RZFBQKCLKJ2MX1AT2ZRZFYRUXTJUMFGA1YUS5ZF",
+      client_secret: "LUY5JVOKL3Y0W0FCHDLIJ4A1DXKJVHKBRDOR4CHCWSRG54TJ",
+      query: "attractions",
+      near: "Pittsburgh",
+      v: "20181011"
+    }
+    // Run: npm install axios
+    axios.get(endPoint + new URLSearchParams(parameters)) 
+      .then(response => {
+        this.setState({
+          venues: response.data.response.groups[0].items
+        }, scriptSrc())
+      })
+      .catch(error => {
+        console.log("Error: " + error)
+      })
+
+  }
+
+  initMap = () => {
+    let google = window.google
+
+    const map = new google.maps.Map(document.getElementById('map') , {
+      center: { lat: 40.448506, lng: -80.00250},
+      zoom: 12
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main>
+        <div id="map">
+        </div>
+      </main>  
     );
   }
 }
 
 export default App;
+
+function scriptSrc() {
+  let index = window.document.getElementsByTagName("script")[0]
+  let script = window.document.createElement("script")
+  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBj5AzHYC1kUPRnvaT6G6zsAONHSpKmoqQ&callback=initMap'
+  script.async = true
+  script.defer = true
+  script.onerror = function() {
+      document.write("Error: Google Maps can't be loaded");
+  }
+  index.parentNode.insertBefore(script, index)
+}
