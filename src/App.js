5d1f6ca91d72a5213d9d593c7a77e731c86a8699
[jkianese] <[jkianese@gmail.com]>
2018-10-14 13:34:48 -0400

    map, makers, infowindow

--- src/App.js
+++ src/App.js
@@ -13,38 +13,78 @@ class App extends Component {
 
   componentDidMount() {
     this.getVenues()
+  }
+
+  // get Google Maps API Key
+  getSource = () => {
+    scriptSource()
     window.initMap = this.initMap
   }
 
+  // fetch venue data from foursquare API using Axios
   getVenues = () => {
     const endPoint = "https://api.foursquare.com/v2/venues/explore?"
     const parameters = {
       client_id: "F1BIU3KU3RZFBQKCLKJ2MX1AT2ZRZFYRUXTJUMFGA1YUS5ZF",
       client_secret: "LUY5JVOKL3Y0W0FCHDLIJ4A1DXKJVHKBRDOR4CHCWSRG54TJ",
-      query: "attractions",
-      near: "Pittsburgh",
-      v: "20181011"
-    }
+      query: "tacos",
+      near: "Pittsburgh, PA",
+      limit: 5,
+      v: "20181014"
+    };
     // Run: npm install axios
-    axios.get(endPoint + new URLSearchParams(parameters)) 
+    axios
+      .get(endPoint + new URLSearchParams(parameters)) 
       .then(response => {
         this.setState({
           venues: response.data.response.groups[0].items
-        }, scriptSrc())
+        }, this.getSource())
       })
       .catch(error => {
         console.log("Error: " + error)
       })
-
   }
 
+  // render map using Google Maps JavaScript API
   initMap = () => {
     let google = window.google
-
+    
+    // Create Map
     const map = new google.maps.Map(document.getElementById('map') , {
       center: { lat: 40.448506, lng: -80.00250},
       zoom: 12
     });
+
+    // Create InfoWindow
+    let infowindow = new google.maps.InfoWindow()
+
+    // Create dynamic animated markers and InfoWindow   
+    this.state.venues.map(myVenues => {
+
+      let contentString = `${myVenues.venue.name}`
+
+      let marker = new google.maps.Marker({
+        position: { lat: myVenues.venue.location.lat, lng: myVenues.venue.location.lng },
+        map: map,
+        Title: myVenues.venue.name,
+        animation: google.maps.Animation.DROP
+      });
+
+      // clickable markers
+      marker.addListener('click', function() {
+        marker
+          .setAnimation(window.google.maps.Animation.BOUNCE);
+          setTimeout(function(){ marker.setAnimation(null); }, 2000);
+        
+        // change content
+        infowindow.setContent(contentString)
+        
+        // open InfoWindow 
+        infowindow.open(map, marker);
+      })
+
+    });
+
   }
 
   render() {
@@ -59,7 +99,7 @@ class App extends Component {
 
 export default App;
 
-function scriptSrc() {
+function scriptSource() {
   let index = window.document.getElementsByTagName("script")[0]
   let script = window.document.createElement("script")
   script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBj5AzHYC1kUPRnvaT6G6zsAONHSpKmoqQ&callback=initMap'
