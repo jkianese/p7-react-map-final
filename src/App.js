65858068a3780bfcd53e147c53c7de0c4fcfb23d
[jkianese] <[jkianese@gmail.com]>
2018-10-14 18:45:33 -0400

    clickable list, markers disappear

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
+import Locations from './components/Locations';
 import axios from 'axios'
 
 class App extends Component {
-
+  
   state = {
-    venues: [],
-    markers: [],
-    query: '',
-    infowindow: []
+        venues: [],
+        markers: [],
+        query: '',
+        infowindow: []
   }  
 
   componentDidMount() {
     this.getVenues()
   }
+
+  updateQuery = query => {
+    this.setState({ query });
+      if(query.trim() === ""){
+    this.state.markers.forEach(marker => marker.setVisible(true)) // markers might have to be allMarkers
+    return true;
+      } else {
+      let venues = this.state.venues.filter(venue => {
+          return venue.venue.name.toLowerCase().indexOf(query.toLowerCase()) > -1
+      })
+      venues
+        .forEach(item => this.state.markers
+        .filter(marker => marker.id !== item.venue.name)
+        .map(falseMarker => falseMarker
+        .setVisible(false)))
+      venues
+        .forEach(item => this.state.markers
+        .filter(marker => marker.id === item.venue.name)
+        .map(trueMarker => trueMarker
+        .setVisible(true)))
+    }
+  }; 
 
   // get Google Maps API Key
   getSource = () => {
@@ -27,7 +50,7 @@ class App extends Component {
     const parameters = {
       client_id: "F1BIU3KU3RZFBQKCLKJ2MX1AT2ZRZFYRUXTJUMFGA1YUS5ZF",
       client_secret: "LUY5JVOKL3Y0W0FCHDLIJ4A1DXKJVHKBRDOR4CHCWSRG54TJ",
-      query: "tacos",
+      query: "arts",
       near: "Pittsburgh, PA",
       limit: 5,
       v: "20181014"
@@ -42,9 +65,24 @@ class App extends Component {
       })
       .catch(error => {
         console.log("Error: " + error)
-      })
+      });
   }
+  
+  showMarkerBox = (marker, contentString) => {
 
+    // var infowindow = new window.google.maps.InfoWindow();
+
+    this.state.infowindow.setContent(contentString);
+    
+    this.state.infowindow.open(this.state.map, marker);
+    /*
+    marker.setAnimation(window.google.maps.Animation.BOUNCE);
+    setTimeout(function() {
+      marker.setAnimation(null);
+    }, 2000);
+    */
+  }
+  
   // render map using Google Maps JavaScript API
   initMap = () => {
     let google = window.google
@@ -57,29 +95,33 @@ class App extends Component {
 
     // Create InfoWindow
     let infowindow = new google.maps.InfoWindow()
+      this.setState({ map, infowindow })
 
     // Create dynamic animated markers and InfoWindow   
-    let allMarkers = this.state.venues.map(myVenues => {
+    let markers = this.state.venues.map(myVenues => {
 
-      let contentString = 
-      `<center> 
+      let contentString = `${myVenues.venue.name}` 
+      /*`<center> 
       ${myVenues.venue.name}<br> 
       ${myVenues.venue.location.address}<br> 
       ${myVenues.venue.location.city}<br>
       ${myVenues.venue.location.state}<br>
       ${myVenues.venue.location.postalCode}<br> 
-      </center>`
-
+      </center>`;
+      */
       let marker = new google.maps.Marker({
         position: { lat: myVenues.venue.location.lat, lng: myVenues.venue.location.lng },
         map: map,
+        id: myVenues.venue.id,
         title: myVenues.venue.name,
         animation: google.maps.Animation.DROP
       });
 
       // clickable markers
-      marker.addListener('click', function() {
+      marker.addListener('click', () => {
         
+        this.showMarkerBox(marker, contentString);
+
         // Animate The Marker
         if (marker.getAnimation() !== null) {
           marker.setAnimation(null);
@@ -97,15 +139,20 @@ class App extends Component {
       })
       return marker;
     });
-    this.setState({ allMarkers });
+    this.setState({ markers });
   }
 
   render() {
     return (
-      <main>
-        <div id="map">
-          
-        </div>
+      <main className="container">
+        <div id="map" /> 
+          <Locations 
+            venues={this.state.venues}
+            query={this.state.query}
+            markers={this.state.markers}
+            updateQuery={this.updateQuery}
+            showMarkerBox={this.showMarkerBox} 
+          />
       </main>  
     );
   }
