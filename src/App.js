b75ec806d318c56777bdcdbcc942a06ee1d79a0f
[jkianese] <[jkianese@gmail.com]>
2018-10-24 22:09:41 -0400

    Sidebar functional, close search bar button

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
-import React, { Component } from 'react';
-import './App.css';
-import Locations from './components/Locations';
-import axios from 'axios'
+import React, { Component } from "react";
+import "./App.css";
+import Locations from "./components/Locations";
+import axios from "axios";
+
+/*
+ 1) I've got it so that the markers filter correctly as input is entered, 
+ however, for some reason when the list is filtered and I click a remaining 
+ marker it defaults incorrectly to the first venue on my list. 2} when I click 
+ on a venue from the sidebar the infowindow shows only the venue's name, I need to 
+ get to same content to show as when I click on a marker. 3} I need to get the map 
+ centered for responsiveness so that it centers correctly on mobile (as it is right 
+  now the sidebar covers the makers on a mobile viewport, which sucks). Thanks so much!!!
+*/
 
 class App extends Component {
-  
   state = {
-        myVenues: [],
-        allMarkers: [],
-        query: '',
-        infowindow: [],
-        selectedVenue: []
-  }  
+    myVenues: [],
+    allMarkers: [],
+    query: "",
+    infowindow: [],
+    selectedVenue: [],
+    map: null
+  };
 
   componentDidMount() {
-    this.getVenues()
+    this.getVenues();
   }
 
   // get Google Maps API Key
   getSource = () => {
-    scriptSource()
-    window.initMap = this.initMap
-  }
+    scriptSource();
+    window.initMap = this.initMap;
+  };
 
   // fetch venue data from Foursquare API using Axios
   getVenues = () => {
-    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?";
     const parameters = {
       client_id: "F1BIU3KU3RZFBQKCLKJ2MX1AT2ZRZFYRUXTJUMFGA1YUS5ZF",
       client_secret: "LUY5JVOKL3Y0W0FCHDLIJ4A1DXKJVHKBRDOR4CHCWSRG54TJ",
@@ -36,129 +46,145 @@ class App extends Component {
     };
     // Run: npm install axios
     axios
-      .get(endPoint + new URLSearchParams(parameters)) 
+      .get(endPoint + new URLSearchParams(parameters))
       .then(response => {
-        this.setState({
-          myVenues: response.data.response.groups[0].items
-        }, this.getSource())
+        this.setState(
+          {
+            myVenues: response.data.response.groups[0].items
+          },
+          this.getSource()
+        );
       })
       .catch(error => {
-        console.log("Error: " + error)
+        console.log("Error: " + error);
       });
-  }
-  
+  };
+
   // render map using Google Maps JavaScript API
   initMap = () => {
-    let google = window.google
+    let google = window.google;
 
-    // Attempt to center on mobile devices -- not going well 
+    // Attempt to center on mobile devices -- not going well
     // const bounds = new google.maps.LatLngBounds()
-    
+
     // Create Map
-    const map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat: 40.448506, lng: -80.00250},
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 40.448506, lng: -80.0025 },
       zoom: 12
     });
 
     // Create InfoWindow
-    let infowindow = new google.maps.InfoWindow()
-      this.setState({ map, infowindow })
+    let infowindow = new google.maps.InfoWindow();
+    this.setState({ map, infowindow });
 
-    // Create dynamic animated markers and InfoWindow   
+    // Create dynamic animated markers and InfoWindow
     let allMarkers = this.state.myVenues.map(venueInfo => {
-      
-      let contentString =  
-      `<center> 
+      let contentString = `<center> 
       ${venueInfo.venue.name}<br> 
       ${venueInfo.venue.location.address}<br> 
       ${venueInfo.venue.location.city}<br>
       ${venueInfo.venue.location.state}<br>
       ${venueInfo.venue.location.postalCode}<br> 
       </center>`;
-      
+
       let marker = new google.maps.Marker({
-        position: { lat: venueInfo.venue.location.lat, lng: venueInfo.venue.location.lng },
+        position: {
+          lat: venueInfo.venue.location.lat,
+          lng: venueInfo.venue.location.lng
+        },
         map: map,
+        address: venueInfo.venue.location.address,
+        city: venueInfo.venue.location.city,
+        state: venueInfo.venue.location.state,
+        postalCode: venueInfo.venue.location.postalCode,
         id: venueInfo.venue.id,
         title: venueInfo.venue.name,
-        animation: google.maps.Animation.DROP,
-    
+        animation: google.maps.Animation.DROP
       });
       // Added to try to center on mobile -- not going well
-      // bounds.extend(marker.position); 
-      // map.fitBounds(bounds); 
+      // bounds.extend(marker.position);
+      // map.fitBounds(bounds);
 
       // clickable markers
-      marker.addListener('click', () => {
-        
-        this.sidebarList(marker, contentString);      
-        
+      marker.addListener("click", () => {
+        this.sidebarList(marker);
+
         // Animate The Marker on click
-          marker.setAnimation(google.maps.Animation.BOUNCE);
-            setTimeout(function() {
-            marker.setAnimation(null);
-          }, 2000);
-        
-      
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+          marker.setAnimation(null);
+        }, 2000);
+
         // change content
-        infowindow.setContent(contentString)
-        
-        // open InfoWindow 
+        infowindow.setContent(contentString);
+
+        // open InfoWindow
         infowindow.open(map, marker);
-      })
+      });
       return marker;
     });
     this.setState({ allMarkers });
-  }
+  };
 
   // Sidebar list of venues
-  sidebarList = (marker, contentString) => {
-
-    this.state.infowindow.setContent(contentString);  
+  sidebarList = marker => {
+    const contentString = `<center> 
+    ${marker.title}<br> 
+    ${marker.address}<br> 
+    ${marker.city}<br>
+    ${marker.state}<br>
+    ${marker.postalCode}<br> 
+    </center>`;
+    console.log(marker);
+    this.state.infowindow.close();
+    this.state.infowindow.setContent(contentString);
     this.state.infowindow.open(this.state.map, marker);
-  
+
     marker.setAnimation(window.google.maps.Animation.BOUNCE);
     setTimeout(function() {
       marker.setAnimation(null);
     }, 2000);
-    
-  }
-  
- // Filter search
- updateQuery = query => {
-  this.setState({ query });
-    if(query.trim() === "") {
-  this.state.allMarkers.forEach(marker => marker.setVisible(true)) 
-  return true;
+  };
+
+  // Filter search
+  updateQuery = query => {
+    this.state.infowindow.close(); // added this to ensure info window closes whenever search field is changed
+
+    this.setState({ query });
+    if (query.trim() === "") {
+      this.state.allMarkers.forEach(marker => marker.setVisible(true));
+      return true;
     } else {
-    let allVenues = this.state.myVenues.filter(venue => {
-        return venue.venue.name.toLowerCase().indexOf(query.toLowerCase()) > -1  
-    })
-    allVenues
-      .forEach(item => this.state.allMarkers
-      .filter(marker => marker.id !== item.venue.id)
-      .map(falseMarker => falseMarker
-      .setVisible(false)))
-    allVenues
-      .forEach(item => this.state.allMarkers
-      .filter(marker => marker.id === item.venue.id)
-      .map(trueMarker => trueMarker  
-      .setVisible(true)))
-  }
-}; 
-  
+      let allVenues = this.state.myVenues.filter(venue => {
+        return venue.venue.name.toLowerCase().indexOf(query.toLowerCase()) > -1;
+      });
+      allVenues.forEach(item =>
+        this.state.allMarkers
+          .filter(marker => marker.id !== item.venue.id)
+          .map(falseMarker => falseMarker.setVisible(false))
+      );
+      allVenues.forEach(item =>
+        this.state.allMarkers
+          .filter(marker => marker.id === item.venue.id)
+          .map(trueMarker => trueMarker.setVisible(true))
+      );
+    }
+  };
+
   render() {
     return (
       <main className="map-container">
-        <div id="map" role="application" aria-label="map"/> 
-          <Locations 
+        <div id="map" role="application" aria-label="map" />
+        <div>
+          <Locations
             myVenues={this.state.myVenues}
             query={this.state.query}
             allMarkers={this.state.allMarkers}
             updateQuery={this.updateQuery}
-            sidebarList={this.sidebarList} 
+            sidebarList={this.sidebarList} // removed info window as it's not used on Locations.js
           />
-      </main>  
+        </div>
+      </main>
     );
   }
 }
@@ -166,13 +192,14 @@ class App extends Component {
 export default App;
 
 function scriptSource() {
-  let index = window.document.getElementsByTagName("script")[0]
-  let script = window.document.createElement("script")
-  script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBj5AzHYC1kUPRnvaT6G6zsAONHSpKmoqQ&callback=initMap'
-  script.async = true
-  script.defer = true
+  let index = window.document.getElementsByTagName("script")[0];
+  let script = window.document.createElement("script");
+  script.src =
+    "https://maps.googleapis.com/maps/api/js?key=AIzaSyBj5AzHYC1kUPRnvaT6G6zsAONHSpKmoqQ&callback=initMap";
+  script.async = true;
+  script.defer = true;
   script.onerror = function() {
-      document.write("Error: Google Maps can't be loaded");
-  }
-  index.parentNode.insertBefore(script, index)
+    document.write("Error: Google Maps can't be loaded");
+  };
+  index.parentNode.insertBefore(script, index);
 }
--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,41 +1,82 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Locations extends Component {
-    
-  onClickLocation = (id, title) => {
-    this.props.sidebarList(this.props.allMarkers[id], title)
-  } 
-  
+  state = {
+    sidebarClicked: false
+  };
+  handleClick = () => {
+    console.log("click");
+    this.setState({
+      sidebarClicked: !this.state.sidebarClicked
+    });
+  };
+  onClickLocation = (venue, title) => {
+    // this method now performs a simple association between the passed venue and the allMarkers
+    // array passed from App.js --> when a match is found, App's `sidebarList` method is passed the matching
+    // marker and title arguments. Logs below were simply for testing
+    console.log(this.props.allMarkers);
+    console.log(this.props.myVenues);
+    this.props.allMarkers.forEach(marker => {
+      if (marker.id === venue.venue.id) {
+        this.props.sidebarList(marker, title);
+      }
+    });
+    /* problem:
+        - when sidebar list is full, index positions align 
+            --> you *could* do this, but it's perhaps not easy. Since you can't say what 
+                any particular search will be, you're essentially saying you'd like for X combinations
+        - when filtered, sidebar list shrinks but allMarkers doesn't
+            --> confirmed it does; must have fixed this in the interim?
+        - content string is accurate, but the marker the info window appears over
+          is based on the index position (faulty) rather than an always-matching marker
+            --> see above
+    */
+  };
+
   render() {
-   
     const { myVenues } = this.props;
-    
+
     return (
-      <div className="sidebar-boxes"> 
-        <input className="text-input"
-          aria-label="search field"
-          type="text"
-          placeholder="Search Places"
-          value={this.props.query}
-          onChange={e => this.props.updateQuery(e.target.value)}
-          
+      <div className="sidebar">
+        <input
+          type="button"
+          value={this.state.sidebarClicked ? "Show" : "Hide"}
+          className="show"
+          onClick={() => this.handleClick()}
         />
-        <ul className="locations-list">
-          {myVenues
-            .filter(filtered =>
-                filtered.venue.name
-            .toLowerCase()
-            .indexOf(this.props.query.toLowerCase()) > -1
-            )
-            .map((item, idx) => {
-              return (
-                <li tabIndex="0" className="locations-names" key={idx} onClick={()=> 
-                    this.onClickLocation(idx, item.venue.name)}>
-                      {item.venue.name}
+        <div
+          style={{ display: this.state.sidebarClicked ? "none" : "block" }}
+          className="sidebar-boxes">
+          <input
+            className="text-input"
+            aria-label="search field"
+            type="text"
+            placeholder="Search Places"
+            value={this.props.query}
+            onChange={e => this.props.updateQuery(e.target.value)}
+          />
+          <ul className="locations-list">
+            {/* modified syntax below to ensure clear identification of parameters/variables */}
+            {myVenues
+              .filter(
+                venue =>
+                  venue.venue.name
+                    .toLowerCase()
+                    .indexOf(this.props.query.toLowerCase()) > -1
+              )
+              .map((venue, idx) => (
+                <li
+                  tabIndex="0"
+                  className="locations-names"
+                  key={idx}
+                  onClick={() => this.onClickLocation(venue, venue.venue.name)}>
+                  {" "}
+                  {/* modified this so that the idx isn't passed, but rather the venue */}
+                  {venue.venue.name}
                 </li>
-              );
-            })}
-        </ul>
+              ))}
+          </ul>
+        </div>
       </div>
     );
   }
